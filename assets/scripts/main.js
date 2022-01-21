56664d9e7b371db9fc5d05d9c881029cccb01b27
Samuel McBroom <sam@sammcb.com>
2022-01-21 16:45:53 -0500

    Increased mobile nav height.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,13 +4,13 @@ let navOpen = false
 function toggleNav() {
   navOpen = !navOpen
   document.getElementById('mobile-toggle').innerHTML = navOpen ? '<i class="fa-solid fa-xmark"></i>' : '<i class="fa-solid fa-bars"></i>'
-  document.getElementById('nav').style.height = navOpen ? '200px' : '0'
+  document.getElementById('nav').style.maxHeight = navOpen ? '250px' : '0'
 }
 
 function updateElements() {
   if (window.innerWidth > maxMobileWidth) {
     navOpen = false
-    document.getElementById('nav').style.height = ''
+    document.getElementById('nav').style.maxHeight = ''
     document.getElementById('mobile-toggle').innerHTML = '<i class="fa-solid fa-bars"></i>'
     stickyNav()
   } else {
